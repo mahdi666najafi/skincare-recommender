91998e4fe9eb722f95980bac1f8a667f832f5893
ArtA <artadnsh@gmail.com>
2025-08-24 14:42:35 +0330

    modified a little

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+
+
 urlpatterns = [
     path('products/', views.ProductList.as_view(), name='product-list'),
-    path('products/<int:product_id>/', views.ProductDetail.as_view(), name='product-detail'),
-    path('interaction/log/', views.LogInteraction.as_view(), name='log-interaction'),
+    path('products/<int:pk>/', views.ProductDetail.as_view(), name='product-detail'),
+    path('interaction-log/', views.LogInteraction.as_view(), name='log-interaction'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+
 from rest_framework.response import Response
 from rest_framework import generics,status
 from .models import Product, BrowsingHistory
 from .serializers import ProductSerializer
-# Create your views here.
+
+
 class ProductList(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
